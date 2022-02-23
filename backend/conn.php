2c26b440859ae5d5ffd7695403e1b6969591c209
TheMaksoo <39918032+TheMaksoo@users.noreply.github.com>
2022-02-23 08:36:02 +0100

    Update conn.php

--- backend/conn.php
+++ backend/conn.php
@@ -4,5 +4,14 @@
 require_once 'config.php';
 
 //Met behulp van PDO zetten we de connectie op, waarna we met setAttribute de manier van errormeldingen weergeven bepalen.
-$conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$quere = "INSERT INTO meldingen (attractie, type) 
+          VALUES(:attractie, :type)";
+          
+$statement  = $conn->prepare($query);
+
+$statement->execute([
+    ":attractie" => $attractie,
+    ":type" => $type,
+    ]);
+
+$items = $statement-> fetchAll(PDO: : FETCH_ASSOC);
