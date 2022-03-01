8d339b04d76bce3592800fe8c4da90326645bd3b
TheMaksoo <39918032+TheMaksoo@users.noreply.github.com>
2022-03-01 02:30:45 +0100

    tabel

--- meldingen/index.php
+++ meldingen/index.php
@@ -19,7 +19,48 @@
             echo "<div class='msg'>" . $_GET['msg'] . "</div>";
         } ?>
 
-        <div style="height: 300px; background: #ededed; display: flex; justify-content: center; align-items: center; color: #666666;">(hier komen de storingsmeldingen)</div>
+        <div style="height: 400px; background: #ededed; display: flex; justify-content: center; align-items: center; color: #666666;">
+        <?php
+            require_once '../backend/conn.php';
+            $query = "SELECT * FROM meldingen";
+            $statement = $conn->prepare($query);
+            $statement->execute();
+            $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+        <table>
+            <tr>
+                <th>Attractie</th>
+                <th>Prioriteit</th>
+                <th>Type</th>
+                <th>Capaciteit</th>
+                <th>Melder</th>
+                <th>Gemeld op</th>
+                <th>Overige info</th>
+            </tr>
+            <?php foreach($meldingen as $melding): ?>
+                <tr>
+                    <td><?php echo $melding['attractie']; ?></td>
+
+                    <td><?php 
+                    if ($melding['prioriteit'] == 1)
+                    {
+                        echo "Ja";
+                    }
+                    else 
+                    {
+                        echo "Nee";
+                    }
+                     ?></td>
+                    <td><?php echo $melding['type']; ?></td>
+                    <td><?php echo $melding['capaciteit']; ?></td>
+                    <td><?php echo $melding['melder']; ?></td>
+                    <td><?php echo $melding['gemeld_op']; ?></td>
+                    <td><?php echo $melding['overige_info']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    
+        </div>
     </div>  
 
 </body>
